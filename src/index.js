0df409bf71418460e7ffe3b12e1850ed9b3849ac
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-01-28 14:58:48 +0200

    added scroll

--- src/index.js
+++ src/index.js
@@ -22,11 +22,6 @@ let lightbox = new SimpleLightbox('.photo-card a', {
   captionDelay: 250,
 });
 
-window.scrollBy({
-  top: 290,
-  behavior: 'smooth',
-});
-
 function onSearch(e) {
   e.preventDefault();
 
@@ -58,6 +53,14 @@ function onSearch(e) {
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * -100,
+        behavior: 'smooth',
+      });
     })
     .catch(error => console.log(error));
 }
